0626d7c8411740d4c9afa77890011d2a4da9565b
Tien-A <tienbluejay@gmail.com>
2024-02-07 22:09:02 +0700

    EZC-136 Add useQuery

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import EZChallenge from './src/screens/EZChallenge';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <EZChallenge />
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <EZChallenge />
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 };
 
