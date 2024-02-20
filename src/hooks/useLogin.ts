9bc3a3448f91e20b322a3cb99f9fc171ef90a35d
Tien-A <tienbluejay@gmail.com>
2024-02-20 11:39:22 +0700

    handle call api login

---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,18 @@
+import {Alert} from 'react-native';
+import {useMutation} from '@tanstack/react-query';
+import axios from 'axios';
+const API_LOGIN = 'http://192.168.132.242:4000/api/login';
+
+export default function useLogin() {
+  const login = useMutation({
+    mutationKey: ['login'],
+    mutationFn: async dataLogin => {
+      const res = await axios.post(API_LOGIN, dataLogin);
+      return res.data;
+    },
+    onSuccess: data => Alert.alert(data.message),
+    onError: data => Alert.alert('Login failed', data.message),
+  });
+
+  return {...login};
+}
