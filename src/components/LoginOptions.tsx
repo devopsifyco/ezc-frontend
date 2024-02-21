7e64340b38bf0585ed64f8d3c77a231aa67b90bc
Tien-A <tienbluejay@gmail.com>
2024-02-21 15:55:37 +0700

    add style for LoginOptions

--- src/components/LoginOptions.tsx
+++ src/components/LoginOptions.tsx
@@ -35,6 +35,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     gap: 10,
+    paddingBottom: 10,
   },
   optionLogin: {
     flexDirection: 'row',
