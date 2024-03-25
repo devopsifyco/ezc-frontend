bcc8d2de3ae5bdbd24ec77cdf076375c8c061570
Tien-A <tienbluejay@gmail.com>
2024-03-25 09:43:11 +0700

    update

--- src/routers/BottomTabs.tsx
+++ src/routers/BottomTabs.tsx
@@ -1,59 +1,52 @@
 import React from 'react';
-import {Alert, Image, Pressable, StyleSheet, View} from 'react-native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { StyleSheet, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import ExploreScreen from '../screens/ExploreScreen';
 import MapScreen from '../screens/MapScreen';
 import EmptyScreen from '../screens/EmtyScreen';
 import ProfileScreen from '../screens/profile';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import {
+  faHouse,
+  faDonate,
+  faLocationDot,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
+import PlusButton from '../components/PlusButton';
 
 const Tab = createBottomTabNavigator();
-const URL_IMAGE = '../assets/bottom_tab/';
 
 export default function BottomTabs() {
   return (
     <>
       <View style={styles.container}>
-        <Pressable
-          style={styles.moreAction}
-          onPress={() => Alert.alert('Hello mày')}>
-          <Image source={require(`${URL_IMAGE}/moreAction.png`)} />
-        </Pressable>
+        <PlusButton />
       </View>
 
       <Tab.Navigator
         initialRouteName="HomeScreen"
-        screenOptions={({route}) => ({
+        screenOptions={({ route }: any) => ({
           // eslint-disable-next-line react/no-unstable-nested-components
-          tabBarIcon: ({focused}) => {
+          tabBarIcon: ({ focused }: any) => {
             let rn = route.name;
-            let imageSource;
+            let icon;
 
             if (rn === 'HomeScreen') {
-              imageSource = require(`${URL_IMAGE}/home.png`);
+              icon = faHouse;
             } else if (rn === 'ExploreScreen') {
-              imageSource = require(`${URL_IMAGE}/calendar2.png`);
+              icon = faDonate;
             } else if (rn === 'MapScreen') {
-              imageSource = require(`${URL_IMAGE}/location.png`);
+              icon = faLocationDot;
             } else if (rn === 'ProfileScreen') {
-              imageSource = require(`${URL_IMAGE}/profile.png`);
+              icon = faUser;
             } else if (rn === 'Space') {
               return null;
             }
 
-            return (
-              <>
-                <Image
-                  source={imageSource}
-                  // eslint-disable-next-line react-native/no-inline-styles
-                  style={{
-                    width: 35,
-                    height: 35,
-                    tintColor: focused ? '#FF890B' : '#D9D9D9',
-                  }}
-                />
-              </>
-            );
+            const iconColor = focused ? '#FF890B' : '#D9D9D9';
+
+            return <FontAwesomeIcon icon={icon} color={iconColor} size={28} />;
           },
           tabBarLabel: () => {
             return null;
@@ -62,11 +55,31 @@ export default function BottomTabs() {
             height: 85,
           },
         })}>
-        <Tab.Screen name="HomeScreen" component={HomeScreen} />
-        <Tab.Screen name="ExploreScreen" component={ExploreScreen} />
-        <Tab.Screen name="Space" component={EmptyScreen} />
-        <Tab.Screen name="MapScreen" component={MapScreen} />
-        <Tab.Screen name="ProfileScreen" component={ProfileScreen} />
+        <Tab.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="ExploreScreen"
+          component={ExploreScreen}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="Space"
+          component={EmptyScreen}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="MapScreen"
+          component={MapScreen}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="ProfileScreen"
+          component={ProfileScreen}
+          options={{ headerShown: false }}
+        />
       </Tab.Navigator>
     </>
   );
@@ -87,5 +100,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  middleScreen: {width: 20},
+  middleScreen: { width: 20 },
 });
